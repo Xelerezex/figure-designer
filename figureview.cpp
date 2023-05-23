adcf1cc1581e5c94a81ba651105b78fbc4bd833c
Nikita Beletsky <nibeletsky@gmail.com>
2023-05-23 11:44:46 +0300

    Добавлены переопределения ивентов с мыши.

--- figureview.cpp
+++ figureview.cpp
@@ -15,3 +15,176 @@ FigureView::FigureView(QGraphicsScene* scene, QWidget* parent)
 
     setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
 }
+
+void FigureView::mousePressEvent(QMouseEvent* event)
+{
+    if (event->button() == Qt::MiddleButton)
+    {
+        middleMouseButtonPress(event);
+    }
+    else if (event->button() == Qt::LeftButton)
+    {
+        leftMouseButtonPress(event);
+    }
+    else if (event->button() == Qt::RightButton)
+    {
+        rightMouseButtonPress(event);
+    }
+    else
+    {
+        QGraphicsView::mousePressEvent(event);
+    }
+}
+
+void FigureView::mouseReleaseEvent(QMouseEvent* event)
+{
+    if (event->button() == Qt::MiddleButton)
+    {
+        middleMouseButtonRelease(event);
+    }
+    else if (event->button() == Qt::LeftButton)
+    {
+        leftMouseButtonRelease(event);
+    }
+    else if (event->button() == Qt::RightButton)
+    {
+        rightMouseButtonRelease(event);
+    }
+    else
+    {
+        QGraphicsView::mouseReleaseEvent(event);
+    }
+}
+
+void FigureView::middleMouseButtonPress(QMouseEvent* event)
+{
+    QMouseEvent* releaseEvent = new QMouseEvent(QEvent::MouseButtonRelease,
+                                                event->localPos(),
+                                                event->screenPos(),
+                                                Qt::LeftButton,
+                                                Qt::NoButton,
+                                                event->modifiers());
+    QGraphicsView::mouseReleaseEvent(releaseEvent);
+
+    setDragMode(QGraphicsView::ScrollHandDrag);
+
+    QMouseEvent* fakeEvent = new QMouseEvent(event->type(),
+                                             event->localPos(),
+                                             event->screenPos(),
+                                             Qt::LeftButton,
+                                             event->buttons() | Qt::LeftButton,
+                                             event->modifiers());
+    QGraphicsView::mousePressEvent(fakeEvent);
+}
+
+void FigureView::middleMouseButtonRelease(QMouseEvent* event)
+{
+    QMouseEvent* fakeEvent = new QMouseEvent(event->type(),
+                                             event->localPos(),
+                                             event->screenPos(),
+                                             Qt::LeftButton,
+                                             event->buttons() | Qt::LeftButton,
+                                             event->modifiers());
+    QGraphicsView::mouseReleaseEvent(fakeEvent);
+    setDragMode(QGraphicsView::NoDrag);
+}
+
+void FigureView::leftMouseButtonPress(QMouseEvent* event)
+{
+    //    QString type       = getItemAtClick(event);
+    //    lastLeftMouseClick = mapToScene(event->pos());
+
+    //    if (type == "Socket")
+    //    {
+    //        if (mode == MODE::NOOP)
+    //        {
+    //            mode = MODE::DRAG_EDGE;
+    //            dragEdgeStart(type);
+    //            return;
+    //        }
+    //    }
+
+    //    if (mode == MODE::DRAG_EDGE)
+    //    {
+    //        if (dragEdgeEnd(type))
+    //        {
+    //            return;
+    //        }
+    //    }
+
+    QGraphicsView::mousePressEvent(event);
+}
+
+void FigureView::leftMouseButtonRelease(QMouseEvent* event)
+{
+    //    QString type = getItemAtClick(event);
+
+    //    if (mode == MODE::DRAG_EDGE)
+    //    {
+    //        if (distanceBetweenClickReleaseOff(event))
+    //        {
+    //            if (dragEdgeEnd(type))
+    //            {
+    //                return;
+    //            }
+    //        }
+    //    }
+
+    QGraphicsView::mouseReleaseEvent(event);
+}
+
+void FigureView::rightMouseButtonPress(QMouseEvent* event)
+{
+    QGraphicsView::mousePressEvent(event);
+}
+//---------------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------------//
+void FigureView::rightMouseButtonRelease(QMouseEvent* event)
+{
+    QGraphicsView::mouseReleaseEvent(event);
+}
+//---------------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------------//
+void FigureView::wheelEvent(QWheelEvent* event)
+{
+    // Вычисялем значения зума:
+    const double		  zoomInFactor	= 1.25;
+    const bool			  zoomClamp		= false;
+    const int			  zoomStep		= 1;
+    int					  zoom			= 10;
+    const QPair<int, int> zoomRange		= {0, 10};
+    bool				  clamped		= false;
+    double				  zoomFactor	= 0;
+    double				  zoomOutFactor = 1 / zoomInFactor;
+
+    if (event->angleDelta().y() > 0)
+    {
+        zoomFactor = zoomInFactor;
+        zoom	   = zoomStep;
+    }
+    else
+    {
+        zoomFactor = zoomOutFactor;
+        zoom	   = zoomStep;
+    }
+
+    if (zoom < zoomRange.first)
+    {
+        zoom	= zoomRange.first;
+        clamped = true;
+    }
+
+    if (zoom > zoomRange.second)
+    {
+        // zoom	= zoomRange.second;
+        clamped = true;
+    }
+
+    // Применяем зум к сцене:
+    if (!clamped || (zoomClamp == false))
+    {
+        scale(zoomFactor, zoomFactor);
+    }
+}
--- figureview.h
+++ figureview.h
@@ -10,6 +10,18 @@ class FigureView : public QGraphicsView
 {
 public:
     explicit FigureView(QGraphicsScene* scene, QWidget* parent = nullptr);
+
+protected:
+    virtual void mousePressEvent(QMouseEvent* event) override;
+    virtual void mouseReleaseEvent(QMouseEvent* event) override;
+    virtual void wheelEvent(QWheelEvent* event) override;
+
+    virtual void middleMouseButtonPress(QMouseEvent* event);
+    virtual void middleMouseButtonRelease(QMouseEvent* event);
+    virtual void leftMouseButtonPress(QMouseEvent* event);
+    virtual void leftMouseButtonRelease(QMouseEvent* event);
+    virtual void rightMouseButtonPress(QMouseEvent* event);
+    virtual void rightMouseButtonRelease(QMouseEvent* event);
 };
 
 #endif // FIGUREVIEW_H
