2eb59382a2a57fb336d2998e4fa8baa152fe1927
Xelerezex <nikitavalderman@gmail.com>
2023-05-23 07:48:50 +0300

    Добавлен Центральный Виджет.

---
+++ centralwidget.cpp
@@ -0,0 +1,11 @@
+#include "centralwidget.h"
+
+CentralWidget::CentralWidget(QWidget *parent)
+    : QWidget(parent)
+    , m_pThread{nullptr}
+{
+}
+
+CentralWidget::~CentralWidget()
+{
+}
---
+++ centralwidget.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef CENTRALWIDGET_H
+#define CENTRALWIDGET_H
+
+class CentralWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    CentralWidget(QWidget *parent = nullptr);
+    ~CentralWidget();
+};
+
+#endif
