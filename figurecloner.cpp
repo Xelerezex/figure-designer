156ac3116394a04c3e580564c69d0f5a2c6864f0
Nikita Beletsky <nibeletsky@gmail.com>
2023-08-16 16:03:03 +0300

    Fixed bug with cloning figures that was rotated.

--- figurecloner.cpp
+++ figurecloner.cpp
@@ -12,46 +12,59 @@
 
 Square* FigureCloner::cloneSquare(QGraphicsItem* item)
 {
+	// Указатель на копируемый объект
 	auto* square = qgraphicsitem_cast<Square*>(item);
+	// Матрица трансформации копируемого объекта
+	QTransform transform{square->transform()};
 
 	// Координаты фигуры
-	const auto figureCoord{square->pos().toPoint() + square->center()};
+	const auto figureCoord{transform.map(square->pos()).toPoint()
+						   + square->center()};
 
 	// Создаем новый объект и настраиваем
 	auto* newSquare = new Square{};
 	newSquare->act(StartDrawing{square->center(), figureCoord});
 	newSquare->act(CompleteDrawing{square->destination()});
 	newSquare->setSelected(true);
-	// Разместить фигуру выше на слой
-	newSquare->setZValue(newSquare->zValue() + 0.1);
+	// Задаем новому объекту матрицу трансформаций старого
+	newSquare->setPos(transform.map(item->pos()));
+	newSquare->setRotation(square->rotation());
 
 	return newSquare;
 }
 
 Rectangle* FigureCloner::cloneRectangle(QGraphicsItem* item)
 {
+	// Указатель на копируемый объект
 	auto* rectangle = qgraphicsitem_cast<Rectangle*>(item);
+	// Матрица трансформации копируемого объекта
+	QTransform transform{rectangle->transform()};
 
 	// Координаты фигуры
-	const auto figureCoord{rectangle->pos().toPoint() + rectangle->leftTop()};
+	const auto figureCoord{transform.map(rectangle->pos()).toPoint()
+						   + rectangle->leftTop()};
 
 	// Создаем новый объект и настраиваем
 	auto* newRectangle = new Rectangle{};
 	newRectangle->act(StartDrawing{rectangle->leftTop(), figureCoord});
 	newRectangle->act(CompleteDrawing{rectangle->destination()});
 	newRectangle->setSelected(true);
-	// Разместить фигуру выше на слой
-	newRectangle->setZValue(newRectangle->zValue() + 0.1);
+	// Задаем новому объекту матрицу трансформаций старого
+	newRectangle->setPos(transform.map(item->pos()));
+	newRectangle->setRotation(rectangle->rotation());
 
 	return newRectangle;
 }
 
 Triangle* FigureCloner::cloneTriangle(QGraphicsItem* item)
 {
+	// Указатель на копируемый объект
 	auto* triangle = qgraphicsitem_cast<Triangle*>(item);
-
+	// Матрица трансформации копируемого объекта
+	QTransform transform{triangle->transform()};
 	// Координаты фигуры
-	const auto firstDotCoord{triangle->pos().toPoint() + triangle->first()};
+	const auto firstDotCoord{transform.map(triangle->pos()).toPoint()
+							 + triangle->first()};
 
 	// Создаем новый объект и настраиваем
 	auto* newTriangle = new Triangle{};
@@ -59,26 +72,31 @@ Triangle* FigureCloner::cloneTriangle(QGraphicsItem* item)
 	newTriangle->act(CompleteDrawing{triangle->second()});
 	newTriangle->act(CompleteDrawing{triangle->third()});
 	newTriangle->setSelected(true);
-	// Разместить фигуру выше на слой
-	newTriangle->setZValue(newTriangle->zValue() + 0.1);
+	// Задаем новому объекту матрицу трансформаций старого
+	newTriangle->setPos(transform.map(item->pos()));
+	newTriangle->setRotation(triangle->rotation());
 
 	return newTriangle;
 }
 
 Circle* FigureCloner::cloneCircle(QGraphicsItem* item)
 {
+	// Указатель на копируемый объект
 	auto* circle = qgraphicsitem_cast<Circle*>(item);
-
+	// Матрица трансформации копируемого объекта
+	QTransform transform{circle->transform()};
 	// Координаты фигуры
-	const auto figureCoord{circle->pos().toPoint() + circle->center()};
+	const auto figureCoord{transform.map(circle->pos()).toPoint()
+						   + circle->center()};
 
 	// Создаем новый объект и настраиваем
 	auto* newCircle = new Circle{};
 	newCircle->act(StartDrawing{circle->center(), figureCoord});
 	newCircle->act(CompleteDrawing{circle->destination()});
 	newCircle->setSelected(true);
-	// Разместить фигуру выше на слой
-	newCircle->setZValue(newCircle->zValue() + 0.1);
+	// Задаем новому объекту матрицу трансформаций старого
+	newCircle->setPos(transform.map(item->pos()));
+	newCircle->setRotation(circle->rotation());
 
 	return newCircle;
 }
