96532d1fe8fc7f64177498db3aee175df5e6dde1
Nikita Beletsky <nibeletsky@gmail.com>
2023-08-18 12:30:50 +0300

    Added main icon to project.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 int main (int argc, char* argv[])
 {
 	QApplication application(argc, argv);
+	application.setWindowIcon(QIcon(":/images/main-icon.png"));
 
-	MainWindow	 window;
+	MainWindow window;
 	window.show();
 
 	return application.exec();
